576d2f09c49313f416d5937ad2bb3e3ea8d16616
Joey Zou <webrules@qq.com>
2026-09-10 01:03:17 +0800

    Add static build and automated GitHub Pages deployment

---
+++ build.mjs
@@ -0,0 +1,9 @@
+import { mkdir, copyFile, cp } from 'node:fs/promises';
+
+const output = new URL('./dist/', import.meta.url);
+await mkdir(output, { recursive: true });
+for (const file of ['index.html', 'style.css']) {
+  await copyFile(new URL(file, import.meta.url), new URL(file, output));
+}
+await cp(new URL('./src/', import.meta.url), new URL('./src/', output), { recursive: true });
+console.log('Static game built in dist/');
